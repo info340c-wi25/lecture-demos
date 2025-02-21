46a1aac26bee805ea4d6a3133d83dfc21f20b3e5
Tim Carlson <timca@arenasports.net>
2025-02-20 21:35:05 -0800

    fix a couple of bugs in shoppingLists

--- react-shopping-lists/src/components/App.jsx
+++ react-shopping-lists/src/components/App.jsx
@@ -1,54 +1,3 @@
-// import { HeaderBar } from "./HeaderBar.jsx";
-// import { ShoppingList } from "./Items.jsx";
-// import { NewItemForm } from "./NewItemForm.jsx";
-
-// export default function App() {
-
-//   const stores = [
-//     { timestamp: 1, name: 'Amazon' },
-//     { timestamp: 2, name: 'Walmart' },
-//     { timestamp: 3, name: 'Target' },
-//     { timestamp: 4, name: 'QFC' },
-//   ];
-
-//   return (
-
-//     <div className="container-fluid d-flex flex-column">
-//       <HeaderBar />
-//       <div className="row flex-grow-1">
-//         <div className="col-3">
-//           <StoreList stores={stores} />
-//         </div>
-//         <div className="col d-flex flex-column">
-//           <ShoppingList />
-//           <NewItemForm stores={stores}/>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// function StoreList(props) {
-//   const { stores } = props;
-
-
-//   return (
-//     <div>
-//       <h2>Store List</h2>
-//       <nav className="bg-secondary text-light py3 h-100">
-//         <ul>
-
-//           <li className='text-light'> {stores[0].name}</li>
-//           <li className='text-light'> {stores[1].name}</li>
-//           <li className='text-light'> {stores[2].name}</li>
-//           <li className='text-light'> {stores[3].name}</li>
-
-//         </ul>
-//       </nav>
-//     </div>
-//   )
-// }
-
 import { HeaderBar } from "./HeaderBar.jsx";
 import { ShoppingList } from "./Items.jsx";
 import { NewItemForm } from "./NewItemForm.jsx";
@@ -114,7 +63,7 @@ export default function App() {
         {/* Main Content */}
         <div className="col d-flex flex-column px-3">
           <ShoppingList itemList={itemList} className="mb-3 flex-grow-1 overflow-auto" />
-          <NewItemForm stores={storeList} addItem={addItem} currentStore={currentStore} className="mt-auto" />
+          <NewItemForm stores={storeList} addItem={addItem} currentStore={currentStore} changeCurrentStore={changeCurrentStore}  className="mt-auto" />
         </div>
       </div>
     </div>
