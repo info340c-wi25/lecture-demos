357dc85152199a2c8d07ddc3c6b37297acacce87
Tim Carlson <timca@arenasports.net>
2025-02-24 20:52:47 -0800

    update the signin page

--- react-chat-router/src/components/SignInPage.jsx
+++ react-chat-router/src/components/SignInPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
 
 import DEFAULT_USERS from '../data/users.json';
 
@@ -14,18 +13,16 @@ export default function SignInPage(props) {
     changeUserFunction(selectedUserObj)
   }
 
-  //convenience
   const userButtons = DEFAULT_USERS.map((userObj) => {
-    if(userObj.userId === currentUser.userId){
-      return null; //don't include!
-    }
+    let classListString = "btn user-icon"
+
     return (
-      <Dropdown.Item className="user-icon" key={userObj.userName} 
+      <button className={classListString} key={userObj.userName} 
         name={userObj.userId} onClick={handleClick}
       >
         <img src={userObj.userImg} alt={userObj.userName + " avatar"} />
-        {userObj.userName}
-      </Dropdown.Item>    )
+      </button>
+    )
   })
 
   return (
@@ -33,16 +30,61 @@ export default function SignInPage(props) {
       <div className="container card-body">
         <p className="lead">Pick a user:</p>
         <div>
-          <Dropdown>
-            <Dropdown.Toggle variant="light">
-              <img src={currentUser.userImg} alt={currentUser.userName + " avatar"} />
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              {userButtons}
-            </Dropdown.Menu>
-          </Dropdown>
+          {userButtons}
         </div>
       </div>
     </div>
   )
 }
+
+// import React from 'react';
+// import Dropdown from 'react-bootstrap/Dropdown';
+
+// import DEFAULT_USERS from '../data/users.json';
+
+// export default function SignInPage(props) {
+//   const { currentUser, changeUserFunction} = props;
+
+//   const handleClick = (event) => {
+//     const whichUser = event.currentTarget.name //access button, not image
+//     console.log(whichUser);
+//     const selectedUserObj = DEFAULT_USERS.filter((userObj) => userObj.userId === whichUser)[0] || DEFAULT_USERS[0] //null user if not found
+
+//     changeUserFunction(selectedUserObj)
+//   }
+
+//   const userButtons = DEFAULT_USERS.map((userObj) => {
+//     let classListString = "btn user-icon"
+//         if(userObj.userId === currentUser.userId){
+//       return null; //don't include!
+//     }
+
+//     return (
+//       <Dropdown.Item className={classListString} key={userObj.userName} 
+//         name={userObj.userId} onClick={handleClick}
+//       >
+//         <img src={userObj.userImg} alt={userObj.userName + " avatar"} />
+//       </Dropdown.Item>
+//     )
+//   })
+
+//   return (
+//     <div className="card bg-light">
+//       <div className="container card-body">
+//         <p className="lead">Pick a user:</p>
+//         <div>
+//         <Dropdown>
+//           <Dropdown.Toggle variant="primary" id="dropdown-basic">
+//             <img src={currentUser.userImg} alt={currentUser.userName + " avatar"} />
+//           </Dropdown.Toggle>
+
+//           <Dropdown.Menu>
+//            {userButtons}
+//           </Dropdown.Menu>
+//     </Dropdown>
+//           {/* {userButtons} */}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
