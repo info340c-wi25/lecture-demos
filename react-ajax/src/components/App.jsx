3a6996a1ad61f7df00240265610ce38e863dfc44
Tim Carlson <timca@arenasports.net>
2025-03-04 08:40:42 -0800

    add a couple more examples

--- react-ajax/src/components/App.jsx
+++ react-ajax/src/components/App.jsx
@@ -392,3 +392,186 @@ export default App;
 // }
 
 // export default App;
+
+// //Slide 35 - async wait method of fetching data
+// import React, { useState, useEffect } from 'react';
+
+// //example GitHub repo data
+// const EXAMPLE_DATA = [
+//   { full_name: "(example) react", html_url: "https://github.com/facebook/react" },
+//   { full_name: "(example) react-bootstrap", html_url: "https://github.com/react-bootstrap/react-bootstrap" },
+//   { full_name: "(example) react-router", html_url: "https://github.com/remix-run/react-router" },
+// ];
+
+
+// function App(props) {
+//   const [stateData, setStateData] = useState([]);
+//   //control form
+//   const [queryInput, setQueryInput] = useState('');
+
+  
+//   useEffect(() => {
+//     fetch('data.json')
+//     .then(function (response) {
+//       const dataPromise = response.json()
+//       return dataPromise;
+//     })
+//     .then(function (data) {
+//       console.log("data", data);
+//       setStateData(data);
+//     })
+ 
+//   }, [])
+
+ 
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   // async wait method of fetching data
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+  
+//     const URL = `https://api.github.com/search/repositories?q=${queryInput}&sort=stars`;
+  
+//     try {
+//       const response = await fetch(URL);
+//       const data = await response.json();
+//       console.log("data", data);
+//       setStateData(data.items);
+//     } catch (error) {
+//       console.log("In the catch block:", error);
+//     } finally {
+//       console.log("In the finally block:");
+//     }
+//   };
+
+//   //render the data
+//   const dataElemArray = stateData.map((repo) => {
+//     return <li key={repo.html_url}><a href={repo.html_url}>{repo.full_name}</a></li>
+//   })
+
+//   console.log("rendering content");
+
+//   return (
+//     <div className="container">
+//       <header><h1>AJAX Demo</h1></header>
+
+//       <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         <input type="hidden" name="sort" value="stars" />
+//         <button type="submit" className="btn btn-primary">Search!</button>
+//       </form>
+
+//       <div className="mt-4">
+//         <h2>Results</h2>
+//         {/* results go here */}
+//         {dataElemArray}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// //Slide 36 - getting data at top of file without effect hook
+// import React, { useState, useEffect } from 'react';
+
+// //example GitHub repo data
+// const EXAMPLE_DATA = [
+//   { full_name: "(example) react", html_url: "https://github.com/facebook/react" },
+//   { full_name: "(example) react-bootstrap", html_url: "https://github.com/react-bootstrap/react-bootstrap" },
+//   { full_name: "(example) react-router", html_url: "https://github.com/remix-run/react-router" },
+// ];
+
+
+// function App(props) {
+//   const [stateData, setStateData] = useState([]);
+//   //control form
+//   const [queryInput, setQueryInput] = useState('');
+
+//    // // no effect hook here
+//     // fetch('data.json')
+//     // .then(function (response) {
+//     //   const dataPromise = response.json()
+//     //   return dataPromise;
+//     // })
+//     // .then(function (data) {
+//     //   console.log("data", data);
+//     //   setStateData(data);
+//     // })
+ 
+  
+
+//   useEffect(() => {
+//     fetch('data.json')
+//     .then(function (response) {
+//       const dataPromise = response.json()
+//       return dataPromise;
+//     })
+//     .then(function (data) {
+//       console.log("data", data);
+//       setStateData(data);
+//     })
+ 
+//   }, [])
+
+
+ 
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   // async wait method of fetching data
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+  
+//     const URL = `https://api.github.com/search/repositories?q=${queryInput}&sort=stars`;
+  
+//     try {
+//       const response = await fetch(URL);
+//       const data = await response.json();
+//       console.log("data", data);
+//       setStateData(data.items);
+//     } catch (error) {
+//       console.log("In the catch block:", error);
+//     } finally {
+//       console.log("In the finally block:");
+//     }
+//   };
+
+//   //render the data
+//   const dataElemArray = stateData.map((repo) => {
+//     return <li key={repo.html_url}><a href={repo.html_url}>{repo.full_name}</a></li>
+//   })
+
+//   console.log("rendering content");
+
+//   return (
+//     <div className="container">
+//       <header><h1>AJAX Demo</h1></header>
+
+//       <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         <input type="hidden" name="sort" value="stars" />
+//         <button type="submit" className="btn btn-primary">Search!</button>
+//       </form>
+
+//       <div className="mt-4">
+//         <h2>Results</h2>
+//         {/* results go here */}
+//         {dataElemArray}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
