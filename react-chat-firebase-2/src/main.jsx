e1f29b9e93a6ac7f831dda7dc16753f14e7cc94e
Tim Carlson <timca@arenasports.net>
2025-03-10 19:40:33 -0700

    update firebase config json object

--- react-chat-firebase-2/src/main.jsx
+++ react-chat-firebase-2/src/main.jsx
@@ -17,12 +17,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAuIRlkrH1YQ9T3YLcarj70OD8N4F6aTgk",
-  authDomain: "foo-delete-a4f73.firebaseapp.com",
-  projectId: "foo-delete-a4f73",
-  storageBucket: "foo-delete-a4f73.firebasestorage.app",
-  messagingSenderId: "716284348433",
-  appId: "1:716284348433:web:3259d592c76b019f5c89b0"
+  apiKey: "AIzaSyBySBD69aYpBzGVjVjpZ_Ua1Xmu5vNmkJ0",
+  authDomain: "info340cwinter25.firebaseapp.com",
+  databaseURL: "https://info340cwinter25-default-rtdb.firebaseio.com",
+  projectId: "info340cwinter25",
+  storageBucket: "info340cwinter25.firebasestorage.app",
+  messagingSenderId: "306863158198",
+  appId: "1:306863158198:web:2855336e2b81e655748717"
 };
 
 // Initialize Firebase
