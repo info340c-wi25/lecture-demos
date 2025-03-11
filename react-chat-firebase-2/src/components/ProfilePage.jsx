cc1a345e64f69937b60fcc3314a8cf383a3d82be
Tim Carlson <timca@arenasports.net>
2025-03-11 09:35:40 -0700

    fix up shared storeage demo

--- react-chat-firebase-2/src/components/ProfilePage.jsx
+++ react-chat-firebase-2/src/components/ProfilePage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 export default function ProfilePage(props) {
+  const { currentUser } = props;
   //convenience
-  const displayName = props.currentUser.userName;
+  const displayName = currentUser.userName;
 
   const [imageFile, setImageFile] = useState(undefined)
-  let initialURL = props.currentUser.userImg;
-  const [imageUrl, setImageUrl] = useState(initialURL)
+  const [imageUrl, setImageUrl] = useState(currentUser.userImg) //initial URL
 
   //image uploading!
   const handleChange = (event) => {
@@ -37,9 +37,9 @@ export default function ProfilePage(props) {
   )
 }
 
-// //Slide 26 and 27
+// //Slide 27 and 28
 // import React, { useState } from 'react';
-
+// import { getApp } from "firebase/app";
 // import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 // import { updateProfile } from 'firebase/auth';
 // import { getDatabase, ref as dbRef, set as firebaseSet } from 'firebase/database';
@@ -61,64 +61,66 @@ export default function ProfilePage(props) {
 //       setImageUrl(URL.createObjectURL(imageFile));
 //     }
 //   }
-//   // // This is the code if you are using the storage DB
-
-//   // const handleImageUpload = async (event) => {
-//   //   //upload the file to the storage db
-//   //   console.log("Uploading", imageFile);
-//   //   const storage = getStorage();
-//   //   const imageRef = ref(storage, "userImages/"+currentUser.userId+".png");
-//   //   await uploadBytes(imageRef, imageFile)
-
-//   //   // get the url to this uploaded file so we can reference it from the web
-//   //   const downloadUrlString = await getDownloadURL(imageRef);
-//   //   console.log(downloadUrlString);
-
-//   //   //put in user profile
-//   //   await updateProfile(currentUser, { photoURL: downloadUrlString} );
-
-//   //   //also put in real time database (for fun)
-//   //   const db = getDatabase();
-//   //   const refString = "userData/"+currentUser.userId+"/imgUrl";
-//   //   console.log(refString);
-//   //   const userImgRef = dbRef(db, "userData/"+currentUser.userId+"/imgUrl")
-//   //   await firebaseSet(userImgRef, downloadUrlString);
-
-//   // }
-
-//   // This is the hack if you are using real time database 
+//   // This is the code if you are using the storage DB
 
 //   const handleImageUpload = async (event) => {
 //     //upload the file to the storage db
 //     console.log("Uploading", imageFile);
 
+//     const storage = getStorage(getApp(), "gs://info340-storage.firebasestorage.app")
+//     // const storage = getStorage();
+//     const imageRef = ref(storage, "demo/userImages/"+currentUser.uid+".png");
+//     await uploadBytes(imageRef, imageFile)
+
+//     // get the url to this uploaded file so we can reference it from the web
+//     const downloadUrlString = await getDownloadURL(imageRef);
+//     console.log(downloadUrlString);
+
+//     //put in user profile
+//     await updateProfile(currentUser, { photoURL: downloadUrlString} );
+
+//     //also put in real time database (for fun)
 //     const db = getDatabase();
-//     const userImgURL = dbRef(db, "userData/" + currentUser.userId + "/imgUrl")
-
-//     //create a new <img> element using JavaScript
-//     const imgElem = new Image();
-//     imgElem.src = imageUrl; //local ObjectURL
-
-//     //create an (offscreen) <canvas> element
-//     const canvas = document.createElement('canvas');
-//     canvas.width = imgElem.width;
-//     canvas.height = imgElem.height;
-
-//     //draw the image onto the canvas
-//     const ctx = canvas.getContext('2d');
-//     ctx.drawImage(imgElem, 0, 0);
-
-//     //convert canvas to a data string
-//     const imageDataString = canvas.toDataURL();
-//     console.log("imageDateString:", imageDataString); //this is the string you're interested in!
-//     console.log("userImgURL: ", userImgURL)
-
-//     //store that string in the database as usual
-//    await firebaseSet(userImgURL, imageDataString);
-//    await updateProfile(currentUser, { photoURL: userImgURL} );
-
+//     const refString = "userData/"+currentUser.userId+"/imgUrl";
+//     console.log(refString);
+//     const userImgRef = dbRef(db, "userData/"+currentUser.userId+"/imgUrl")
+//     await firebaseSet(userImgRef, downloadUrlString);
 
 //   }
+
+//   // // This is the hack if you are using real time database 
+
+//   // const handleImageUpload = async (event) => {
+//   //   //upload the file to the storage db
+//   //   console.log("Uploading", imageFile);
+
+//   //   const db = getDatabase();
+//   //   const userImgURL = dbRef(db, "userData/" + currentUser.userId + "/imgUrl")
+
+//   //   //create a new <img> element using JavaScript
+//   //   const imgElem = new Image();
+//   //   imgElem.src = imageUrl; //local ObjectURL
+
+//   //   //create an (offscreen) <canvas> element
+//   //   const canvas = document.createElement('canvas');
+//   //   canvas.width = imgElem.width;
+//   //   canvas.height = imgElem.height;
+
+//   //   //draw the image onto the canvas
+//   //   const ctx = canvas.getContext('2d');
+//   //   ctx.drawImage(imgElem, 0, 0);
+
+//   //   //convert canvas to a data string
+//   //   const imageDataString = canvas.toDataURL();
+//   //   console.log("imageDateString:", imageDataString); //this is the string you're interested in!
+//   //   console.log("userImgURL: ", userImgURL)
+
+//   //   //store that string in the database as usual
+//   //  await firebaseSet(userImgURL, imageDataString);
+//   //  await updateProfile(currentUser, { photoURL: userImgURL} );
+
+
+//   // }
 
 //   return (
 //     <div className="container">
